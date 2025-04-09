dacbe6f5c95bf006615dedd217581a2824bef486
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-04-09 22:12:26 +0530

    Force push from new folder

---
+++ XmlWebDemo/src/main/java/in/sp/backend/Login.java
@@ -0,0 +1,18 @@
+package in.sp.backend;
+
+import java.io.IOException;
+
+import jakarta.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("running huh?!");
+	}
+	
+}
